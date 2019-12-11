55bfa39eb2681c0b451be2f51226e586213f67c1
Nick Imperial <njimperial@gmail.com>
2019-12-11 11:24:03 -0800

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Nick Imperial on 12/11/19.
+//  Copyright © 2019 Nick Imperial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
